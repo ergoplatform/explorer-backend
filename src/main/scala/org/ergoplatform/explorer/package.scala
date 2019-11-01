e17143ea65b773d6a927bbe8bc81dfb5ee4eb375
oskin1 <ilya.arcadich@gmail.com>
2019-11-01 23:06:24 +0300

    Tests fixed.

--- src/main/scala/org/ergoplatform/explorer/package.scala
+++ src/main/scala/org/ergoplatform/explorer/package.scala
@@ -1,52 +1,44 @@
 package org.ergoplatform
 
 import doobie.util.{Get, Put}
-import doobie.refined.implicits._
 import eu.timepit.refined._
 import eu.timepit.refined.api.Refined
 import eu.timepit.refined.string._
-import io.estatico.newtype.{Coercible, NewType}
+import io.estatico.newtype.NewType
 import io.estatico.newtype.ops._
 
 package object explorer {
-
-  sealed class DoobieInstances[A: Get: Put] {
-    implicit def get[T: Coercible[A, *]]: Get[T] = Get[A].map(_.coerce)
-    implicit def put[T]: Put[T] = Put[A].contramap(Coercible.instance(_))
-  }
-
-  object StringTagDoobieInstances extends DoobieInstances[String]
 
   type Id = Id.Type
 
   object Id extends NewType.Default[String] {
     // doobie instances
-    implicit def get: Get[Type] = StringTagDoobieInstances.get
-    implicit def put: Put[Type] = StringTagDoobieInstances.put
+    implicit def get: Get[Type] = Get[String].map(_.coerce)
+    implicit def put: Put[Type] = Put[String].contramap(unwrap(_))
   }
 
   type TxId = TxId.Type
 
   object TxId extends NewType.Default[String] {
     // doobie instances
-    implicit def get: Get[Type] = StringTagDoobieInstances.get
-    implicit def put: Put[Type] = StringTagDoobieInstances.put
+    implicit def get: Get[Type] = Get[String].map(_.coerce)
+    implicit def put: Put[Type] = Put[String].contramap(unwrap(_))
   }
 
   type BoxId = BoxId.Type
 
   object BoxId extends NewType.Default[String] {
     // doobie instances
-    implicit def get: Get[Type] = StringTagDoobieInstances.get
-    implicit def put: Put[Type] = StringTagDoobieInstances.put
+    implicit def get: Get[Type] = Get[String].map(_.coerce)
+    implicit def put: Put[Type] = Put[String].contramap(unwrap(_))
   }
 
   type AssetId = AssetId.Type
 
   object AssetId extends NewType.Default[String] {
     // doobie instances
-    implicit def get: Get[Type] = StringTagDoobieInstances.get
-    implicit def put: Put[Type] = StringTagDoobieInstances.put
+    implicit def get: Get[Type] = Get[String].map(_.coerce)
+    implicit def put: Put[Type] = Put[String].contramap(unwrap(_))
   }
 
   type Address = String Refined Base58StringP
--- src/test/scala/org/ergoplatform/explorer/persistence/RealDbTest.scala
+++ src/test/scala/org/ergoplatform/explorer/persistence/RealDbTest.scala
@@ -3,6 +3,7 @@ package org.ergoplatform.explorer.persistence
 import cats.effect.IO
 import com.dimafeng.testcontainers.PostgreSQLContainer
 import doobie.util.transactor.Transactor
+import doobie.implicits._
 import org.ergoplatform.explorer.CatsInstances
 import org.flywaydb.core.Flyway
 import org.scalactic.source.Position
@@ -33,16 +34,19 @@ trait RealDbTest
     flyway.setSqlMigrationSeparator("__")
     flyway.setLocations("classpath:db")
     flyway.setDataSource(container.jdbcUrl, container.username, container.password)
-    flyway.clean()
     flyway.migrate()
   }
 
   override def afterAll(): Unit =
     container.container.stop()
 
-  override def before(fun: => Any)(implicit pos: Position): Unit =
-    flyway.migrate()
-
   override def after(fun: => Any)(implicit pos: Position): Unit =
-    flyway.clean()
+    truncateAll()
+
+  private def truncateAll(): Unit =
+    sql"""
+         |truncate node_inputs;
+         |truncate node_outputs;
+         |truncate node_headers, node_extensions, node_ad_proofs, blocks_info, node_transactions;
+         |""".stripMargin.update.run.transact(xa).unsafeRunSync()
 }
