bff94ba9dbafacfb3912e593ec0c8a5ff8e9dfa5
oskin1 <ilya.arcadich@gmail.com>
2019-11-01 21:53:30 +0300

    More HeaderRepo specs.

--- src/test/scala/org/ergoplatform/explorer/persistence/repositories/HeaderRepoSpec.scala
+++ src/test/scala/org/ergoplatform/explorer/persistence/repositories/HeaderRepoSpec.scala
@@ -13,11 +13,41 @@ class HeaderRepoSpec
 
   import org.ergoplatform.explorer.persistence.models.Generators._
 
-  property("insert") {
+  property("insert/get") {
+    withHeaderRepoLive { repo =>
+      withSingleInstance(headerGen) { header =>
+        repo.get(header.id).unsafeRunSync() shouldBe None
+        repo.insert(header).unsafeRunSync()
+        repo.get(header.id).unsafeRunSync() shouldBe Some(header)
+      }
+    }
+  }
+
+  property("update") {
     withHeaderRepoLive { repo =>
       withSingleInstance(headerGen) { header =>
         repo.insert(header).unsafeRunSync()
-        repo.get(header.id).unsafeRunSync() shouldBe Some(header)
+        val header2 = header.copy(height = header.height + 1)
+        repo.update(header2).unsafeRunSync()
+        repo.get(header.id).unsafeRunSync() shouldBe Some(header2)
+      }
+    }
+  }
+
+  property("getAllByHeight") {
+    withHeaderRepoLive { repo =>
+      withSingleInstance(headerGen) { header =>
+        repo.insert(header).unsafeRunSync()
+        repo.getAllByHeight(header.height).unsafeRunSync() shouldBe List(header)
+      }
+    }
+  }
+
+  property("getHeightOf") {
+    withHeaderRepoLive { repo =>
+      withSingleInstance(headerGen) { header =>
+        repo.insert(header).unsafeRunSync()
+        repo.getHeightOf(header.id).unsafeRunSync() shouldBe Some(header.height)
       }
     }
   }
