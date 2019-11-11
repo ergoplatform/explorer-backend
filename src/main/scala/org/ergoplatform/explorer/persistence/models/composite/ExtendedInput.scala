d8d9eb1636a0ec5a443862528ba88ac3397cb69e
oskin1 <ilya.arcadich@gmail.com>
2019-11-11 22:32:45 +0300

    Composite entity annotations.

--- src/main/scala/org/ergoplatform/explorer/persistence/models/composite/ExtendedInput.scala
+++ src/main/scala/org/ergoplatform/explorer/persistence/models/composite/ExtendedInput.scala
@@ -3,6 +3,8 @@ package org.ergoplatform.explorer.persistence.models.composite
 import org.ergoplatform.explorer.persistence.models.Input
 import org.ergoplatform.explorer.{Address, TxId}
 
+/** Input entity enriched with a data from corresponding output.
+  */
 final case class ExtendedInput(
   input: Input,
   value: Option[Long],
--- src/main/scala/org/ergoplatform/explorer/persistence/models/composite/ExtendedOutput.scala
+++ src/main/scala/org/ergoplatform/explorer/persistence/models/composite/ExtendedOutput.scala
@@ -3,6 +3,8 @@ package org.ergoplatform.explorer.persistence.models.composite
 import org.ergoplatform.explorer.TxId
 import org.ergoplatform.explorer.persistence.models.Output
 
+/** Output entity enriched with a data from corresponding transaction and header.
+  */
 final case class ExtendedOutput(
   output: Output,
   spentByOpt: Option[TxId],
