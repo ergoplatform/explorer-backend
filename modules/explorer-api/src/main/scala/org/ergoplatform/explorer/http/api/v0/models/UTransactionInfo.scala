3abe01f45b22f6514015fd054dfb0da71a188bf0
oskin1 <ilya.arcadich@gmail.com>
2020-01-19 23:09:39 +0300

    UTransaction smart constructors impl.

--- modules/explorer-api/src/main/scala/org/ergoplatform/explorer/http/api/v0/models/UTransactionInfo.scala
+++ modules/explorer-api/src/main/scala/org/ergoplatform/explorer/http/api/v0/models/UTransactionInfo.scala
@@ -1,9 +1,14 @@
 package org.ergoplatform.explorer.http.api.v0.models
 
+import cats.instances.list._
+import cats.instances.option._
+import cats.syntax.traverse._
+import cats.syntax.option._
+import org.ergoplatform.explorer.TxId
 import org.ergoplatform.explorer.db.models.{UAsset, UInput, UOutput, UTransaction}
 
 final case class UTransactionInfo(
-  id: String,
+  id: TxId,
   inputs: List[UInputInfo],
   outputs: List[UOutputInfo],
   creationTimestamp: Long,
@@ -17,12 +22,34 @@ object UTransactionInfo {
     ins: List[UInput],
     outs: List[UOutput],
     assets: List[UAsset]
-  ): UTransactionInfo = ???
+  ): UTransactionInfo = {
+    val inputsInfo  = ins.map(UInputInfo.apply)
+    val outputsInfo = outs.map(UOutputInfo(_, assets))
+    new UTransactionInfo(tx.id, inputsInfo, outputsInfo, tx.creationTimestamp, tx.size)
+  }
 
   def batch(
     txs: List[UTransaction],
     ins: List[UInput],
     outs: List[UOutput],
     assets: List[UAsset]
-  ): List[UTransactionInfo] = ???
+  ): List[UTransactionInfo] = {
+    val assetsByBox = assets.groupBy(_.boxId)
+    val inputsByTx  = ins.groupBy(_.txId)
+    val outsByTx    = outs.groupBy(_.txId)
+    txs
+      .traverse { tx =>
+        for {
+          inputs  <- inputsByTx.get(tx.id)
+          outputs <- outsByTx.get(tx.id)
+          assets  <- outputs
+                       .foldLeft(List.empty[UAsset]) { (acc, o) =>
+                         assetsByBox.get(o.boxId).toList.flatten ++ acc
+                       }
+                       .some
+        } yield apply(tx, inputs, outputs, assets)
+      }
+      .toList
+      .flatten
+  }
 }
