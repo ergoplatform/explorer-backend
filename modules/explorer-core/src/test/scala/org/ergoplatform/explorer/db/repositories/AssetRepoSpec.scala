bc041de4e3a9c0008b7b2d8121c7e904c573ba54
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2020-01-29 11:28:23 +0200

    formatting;

--- modules/explorer-core/src/test/scala/org/ergoplatform/explorer/db/repositories/AssetRepoSpec.scala
+++ modules/explorer-core/src/test/scala/org/ergoplatform/explorer/db/repositories/AssetRepoSpec.scala
@@ -2,8 +2,8 @@ package org.ergoplatform.explorer.db.repositories
 
 import cats.effect.Sync
 import doobie.ConnectionIO
-import org.ergoplatform.explorer.{BoxId, db}
-import org.ergoplatform.explorer.db.{RealDbTest, repositories}
+import org.ergoplatform.explorer.{db, BoxId}
+import org.ergoplatform.explorer.db.{repositories, RealDbTest}
 import org.ergoplatform.explorer.db.algebra.LiftConnectionIO
 import org.ergoplatform.explorer.db.models.aggregates.ExtendedOutput
 import org.ergoplatform.explorer.db.syntax.runConnectionIO._
@@ -35,20 +35,20 @@ class AssetRepoSpec
   }
 
   property("insert/getIssuingBoxes") {
-    withLiveRepos[ConnectionIO] { (assetRepo, outputRepo, inputRepo)=>
+    withLiveRepos[ConnectionIO] { (assetRepo, outputRepo, inputRepo) =>
       assetRepo.getAllIssuingBoxes.runWithIO() shouldBe empty
 
-      val out = outputGen(true).sample.get
+      val out   = outputGen(true).sample.get
       val asset = assetGen.sample.get.copy(boxId = out.boxId)
       import io.estatico.newtype.ops._
       val inputBoxId = asset.tokenId.toString.coerce[BoxId]
-      val input = inputGen(true).sample.get.copy(txId = out.txId, boxId = inputBoxId)
+      val input      = inputGen(true).sample.get.copy(txId = out.txId, boxId = inputBoxId)
       inputRepo.insert(input).runWithIO()
       assetRepo.insert(asset).runWithIO()
       outputRepo.insert(out).runWithIO()
 
       assetRepo.getAllIssuingBoxes.runWithIO() should
-        contain theSameElementsAs List(ExtendedOutput(out, None))
+      contain theSameElementsAs List(ExtendedOutput(out, None))
     }
   }
 
@@ -57,10 +57,13 @@ class AssetRepoSpec
   ): Any =
     body(db.repositories.AssetRepo[D])
 
-  private def withLiveRepos[D[_]: LiftConnectionIO: Sync](body:
-                                                          (AssetRepo[D, fs2.Stream],
-                                                            OutputRepo[D, fs2.Stream],
-                                                            InputRepo[D]) => Any): Any =
-    body(db.repositories.AssetRepo[D], db.repositories.OutputRepo[D], db.repositories.InputRepo[D])
+  private def withLiveRepos[D[_]: LiftConnectionIO: Sync](
+    body: (AssetRepo[D, fs2.Stream], OutputRepo[D, fs2.Stream], InputRepo[D]) => Any
+  ): Any =
+    body(
+      db.repositories.AssetRepo[D],
+      db.repositories.OutputRepo[D],
+      db.repositories.InputRepo[D]
+    )
 
 }
