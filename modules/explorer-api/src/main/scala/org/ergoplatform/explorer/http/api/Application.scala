171dc6a601039a53e20756a3279415ea8da145b5
oskin1 <ilya.arcadich@gmail.com>
2020-02-16 16:15:30 +0300

    Complete APIv0 app.

--- modules/explorer-api/src/main/scala/org/ergoplatform/explorer/http/api/Application.scala
+++ modules/explorer-api/src/main/scala/org/ergoplatform/explorer/http/api/Application.scala
@@ -1,23 +1,20 @@
 package org.ergoplatform.explorer.http.api
 
 import cats.effect.{ExitCode, Resource}
-import cats.syntax.functor._
 import cats.free.Free.catsFreeMonadForFree
+import cats.syntax.functor._
 import doobie.free.connection.ConnectionIO
 import monix.eval.{Task, TaskApp}
 import org.ergoplatform.ErgoAddressEncoder
 import org.ergoplatform.explorer.db.DbTrans
 import org.ergoplatform.explorer.http.api.settings.Settings
 import org.ergoplatform.explorer.http.api.v0.HttpApiV0
-import org.ergoplatform.explorer.settings.pureConfigInstances._
-import pureconfig.ConfigSource
 import pureconfig.generic.auto._
-import pureconfig.module.catseffect._
 
 object Application extends TaskApp {
 
   def run(args: List[String]): Task[ExitCode] =
-    resources.use {
+    resources(args.headOption).use {
       case (settings, xa) =>
         implicit val e: ErgoAddressEncoder = settings.protocolSettings.addressEncoder
         HttpApiV0[Task, ConnectionIO](settings.httpSettings)(xa)
@@ -25,9 +22,9 @@ object Application extends TaskApp {
           .as(ExitCode.Success)
     }
 
-  private def resources =
+  private def resources(configPathOpt: Option[String]) =
     for {
-      settings <- Resource.liftF(ConfigSource.default.loadF[Task, Settings])
+      settings <- Resource.liftF(Settings.load(configPathOpt))
       xa       <- DbTrans[Task](settings.dbSettings).map(_.trans)
     } yield (settings, xa)
 }
--- modules/explorer-api/src/main/scala/org/ergoplatform/explorer/http/api/settings/Settings.scala
+++ modules/explorer-api/src/main/scala/org/ergoplatform/explorer/http/api/settings/Settings.scala
@@ -1,9 +1,23 @@
 package org.ergoplatform.explorer.http.api.settings
 
+import cats.effect.Sync
 import org.ergoplatform.explorer.settings.{DbSettings, ProtocolSettings}
+import pureconfig.ConfigSource
+import pureconfig.generic.auto._
+import pureconfig.module.catseffect._
+import org.ergoplatform.explorer.settings.pureConfigInstances._
 
 final case class Settings(
   httpSettings: HttpSettings,
   dbSettings: DbSettings,
   protocolSettings: ProtocolSettings
 )
+
+object Settings {
+
+  def load[F[_]: Sync](pathOpt: Option[String]): F[Settings] =
+    pathOpt
+      .map(ConfigSource.file)
+      .getOrElse(ConfigSource.default)
+      .loadF[F, Settings]
+}
