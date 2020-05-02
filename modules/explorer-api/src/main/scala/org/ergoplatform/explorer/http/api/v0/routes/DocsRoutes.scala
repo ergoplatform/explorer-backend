09899a53dd64b81469d66fa90fad042d0d44b109
oskin1 <ilya.arcadich@gmail.com>
2020-05-02 13:16:08 +0300

    Tags added to Openapi spec.

--- modules/explorer-api/src/main/scala/org/ergoplatform/explorer/http/api/v0/routes/DocsRoutes.scala
+++ modules/explorer-api/src/main/scala/org/ergoplatform/explorer/http/api/v0/routes/DocsRoutes.scala
@@ -3,10 +3,12 @@ package org.ergoplatform.explorer.http.api.v0.routes
 import cats.effect.{ContextShift, Sync}
 import cats.syntax.applicative._
 import cats.syntax.either._
+import cats.syntax.option._
 import org.ergoplatform.explorer.http.api.ApiErr
 import org.ergoplatform.explorer.http.api.v0.defs._
 import org.http4s.HttpRoutes
 import sttp.tapir.docs.openapi._
+import sttp.tapir.openapi.Tag
 import sttp.tapir.openapi.circe.yaml._
 import sttp.tapir.server.http4s._
 
@@ -28,10 +30,24 @@ final class DocsRoutes[F[_]: Sync: ContextShift](implicit opts: Http4sServerOpti
     StatsEndpointDefs.endpoints ++
     DocsEndpointDefs.endpoints
 
+  private def tags =
+    Tag("info", "General network info".some) ::
+    Tag("transactions", "Transactions info".some) ::
+    Tag("addresses", "Addresses info, balances etc.".some) ::
+    Tag("assets", "Assets info".some) ::
+    Tag("blocks", "Blocks info".some) ::
+    Tag("charts", "Charts".some) ::
+    Tag("stats", "Network statistics".some) ::
+    Tag("dex", "Dex orders info".some) ::
+    Tag("search", "Search any entity".some) ::
+    Tag("docs", "API documentation".some) ::
+    Nil
+
   private def openApiSpecR: HttpRoutes[F] =
     apiSpecDef.toRoutes { _ =>
       allEndpoints
         .toOpenAPI("Ergo Explorer API v0", "1.0")
+        .tags(tags)
         .toYaml
         .asRight[ApiErr]
         .pure[F]
