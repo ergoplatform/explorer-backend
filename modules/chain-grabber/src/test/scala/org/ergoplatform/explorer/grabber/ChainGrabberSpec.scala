8d0194ad56fe9f6796bb4255f5c44983ed691acb
oskin1 <ilya.arcadich@gmail.com>
2020-07-19 15:40:12 +0300

    Grabber spec fixed.

--- modules/chain-grabber/src/test/scala/org/ergoplatform/explorer/grabber/ChainGrabberSpec.scala
+++ modules/chain-grabber/src/test/scala/org/ergoplatform/explorer/grabber/ChainGrabberSpec.scala
@@ -8,7 +8,7 @@ import org.ergoplatform.explorer.db.algebra.LiftConnectionIO
 import org.ergoplatform.explorer.db.{repositories, RealDbTest}
 import org.ergoplatform.explorer.db.repositories.{HeaderRepo, TransactionRepo}
 import org.ergoplatform.explorer.grabber.GrabberTestNetworkClient.Source
-import org.ergoplatform.explorer.protocol.models.ApiFullBlock
+import org.ergoplatform.explorer.protocol.models.{ApiFullBlock, ApiTransaction}
 import org.ergoplatform.explorer.settings.GrabberAppSettings
 import org.ergoplatform.explorer.testSyntax.runConnectionIO._
 import org.scalacheck.ScalacheckShapeless._
@@ -31,16 +31,14 @@ class ChainGrabberSpec
   private lazy val settings =
     GrabberAppSettings(1.second, mainnetNodes, dbSettings, protocolSettings)
 
-  ignore("Network scanning") {
-    forAll(consistentChainGen(12)) { apiBlocks =>
-      whenever(apiBlocks.map(_.transactions.transactions).forall(_.nonEmpty)) {
-        withLiveRepo[ConnectionIO] { repo =>
-          val networkService = new GrabberTestNetworkClient[IO](Source(apiBlocks))
-          ChainGrabber[IO, ConnectionIO](settings, networkService)(xa.trans)
-            .flatMap(_.run.take(1L).compile.drain)
-            .unsafeRunSync()
-          repo.getBestHeight.runWithIO() shouldBe 11
-        }
+  property("Network scanning") {
+    forSingleInstance(consistentChainGen(12)) { apiBlocks =>
+      withLiveRepo[ConnectionIO] { repo =>
+        val networkService = new GrabberTestNetworkClient[IO](Source(apiBlocks))
+        ChainGrabber[IO, ConnectionIO](settings, networkService)(xa.trans)
+          .flatMap(_.run.take(11L).compile.drain)
+          .unsafeRunSync()
+        repo.getBestHeight.runWithIO() shouldBe 11
       }
     }
   }
@@ -51,36 +49,40 @@ class ChainGrabberSpec
     body(repositories.HeaderRepo[IO, D].unsafeRunSync())
 
   private def consistentChainGen(length: Int): Gen[List[ApiFullBlock]] =
-    Gen
-      .listOfN(length, implicitly[Arbitrary[ApiFullBlock]].arbitrary)
-      .map { rawBlocks =>
-        rawBlocks
-          .foldLeft(List.empty[ApiFullBlock]) {
-            case (Nil, block) =>
-              List(block)
-            case (acc @ parent :: _, block) =>
-              val blockUpd = block
-                .lens(_.header.parentId)
-                .modify(_ => parent.header.id)
-              blockUpd +: acc
-          }
-          .reverse
-          .zipWithIndex
-          .map {
-            case (block, idx) =>
-              val blockId = block.header.id
-              block
-                .lens(_.header.height)
-                .modify(_ => idx)
-                .lens(_.header.minerPk)
-                .modify(_ => MainNetMinerPk)
-                .lens(_.extension.headerId)
-                .modify(_ => blockId)
-                .lens(_.transactions.headerId)
-                .modify(_ => blockId)
-                .lens(_.adProofs)
-                .modify(_.map(_.copy(headerId = blockId)))
-          }
-      }
+    for {
+      blocks <- Gen.listOfN(length, implicitly[Arbitrary[ApiFullBlock]].arbitrary)
+      txs    <- Gen.listOfN(length * 5, implicitly[Arbitrary[ApiTransaction]].arbitrary)
+      blocksWithTxs = txs.grouped(5).toList.zip(blocks).map {
+                        case (txs, b) => b.lens(_.transactions.transactions).modify(_ ++ txs)
+                      }
+    } yield linkRawBlocks(blocksWithTxs)
 
+  private def linkRawBlocks(blocks: List[ApiFullBlock]) =
+    blocks
+      .foldLeft(List.empty[ApiFullBlock]) {
+        case (Nil, block) =>
+          List(block)
+        case (acc @ parent :: _, block) =>
+          val blockUpd = block
+            .lens(_.header.parentId)
+            .modify(_ => parent.header.id)
+          blockUpd +: acc
+      }
+      .reverse
+      .zipWithIndex
+      .map {
+        case (block, idx) =>
+          val blockId = block.header.id
+          block
+            .lens(_.header.height)
+            .modify(_ => idx)
+            .lens(_.header.minerPk)
+            .modify(_ => MainNetMinerPk)
+            .lens(_.extension.headerId)
+            .modify(_ => blockId)
+            .lens(_.transactions.headerId)
+            .modify(_ => blockId)
+            .lens(_.adProofs)
+            .modify(_.map(_.copy(headerId = blockId)))
+      }
 }
