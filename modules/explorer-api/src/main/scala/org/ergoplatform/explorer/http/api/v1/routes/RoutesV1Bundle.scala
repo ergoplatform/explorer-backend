5e2466941472306186cdce6d1ecaff6b0b5a35b3
oskin1 <ilya.arcadich@gmail.com>
2021-06-04 13:28:07 +0300

    Duplicated method deprecated

--- modules/explorer-api/src/main/scala/org/ergoplatform/explorer/http/api/v1/routes/RoutesV1Bundle.scala
+++ modules/explorer-api/src/main/scala/org/ergoplatform/explorer/http/api/v1/routes/RoutesV1Bundle.scala
@@ -43,7 +43,7 @@ object RoutesV1Bundle {
       epochsRoutes    = EpochsRoutes(epochs)
       blocksRoutes    = BlocksRoutes(requestsSettings, blocks)
       tokensRoutes    = TokensRoutes(requestsSettings, tokens)
-      assetsRoutes    = AssetsRoutes(requestsSettings, assets)
+      assetsRoutes    = AssetsRoutes(requestsSettings, assets, tokens)
       txsRoutes       = TransactionsRoutes(requestsSettings, transactions)
       addressesRoutes = AddressesRoutes(requestsSettings, transactions, addresses)
       docs            = DocsRoutes(requestsSettings)
