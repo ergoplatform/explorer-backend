f80cb1c74892a8d43ebd278c95200378aad93ac4
キャンディの神 <monyeodavid@gmail.com>
2021-12-28 18:26:59 +0100

    ADDR. API v1 - Add TokenType to BalanceConfirmed & BalanceTotal (#167) (#169)

--- modules/explorer-core/src/main/scala/org/ergoplatform/explorer/db/models/aggregates/AggregatedAsset.scala
+++ modules/explorer-core/src/main/scala/org/ergoplatform/explorer/db/models/aggregates/AggregatedAsset.scala
@@ -1,10 +1,11 @@
 package org.ergoplatform.explorer.db.models.aggregates
 
-import org.ergoplatform.explorer.TokenId
+import org.ergoplatform.explorer.{TokenId, TokenType}
 
 final case class AggregatedAsset(
   tokenId: TokenId,
   totalAmount: Long,
   name: Option[String],
-  decimals: Option[Int]
+  decimals: Option[Int],
+  `type`: Option[TokenType]
 )
