5b0dc5a3cb36cc0e23647ad23b4b77f52b8652cf
oskin1 <ilya.arcadich@gmail.com>
2022-02-04 11:32:15 +0300

    Explicit CORS.

--- modules/explorer-api/src/main/scala/org/ergoplatform/explorer/http/api/HttpApi.scala
+++ modules/explorer-api/src/main/scala/org/ergoplatform/explorer/http/api/HttpApi.scala
@@ -39,7 +39,7 @@ object HttpApi {
       v0 <- Resource.eval(RoutesV0Bundle(settings.protocol, settings.utxCache, redis)(trans))
       v1 <- Resource.eval(RoutesV1Bundle(settings.service, settings.requests, settings.utxCache, redis)(trans))
       routes     = v0.routes <+> v1.routes
-      corsRoutes = CORS(routes)
+      corsRoutes = CORS(routes, CORS.DefaultCORSConfig)
       http <- BlazeServerBuilder[F](ec)
                 .bindHttp(settings.http.port, settings.http.host)
                 .withHttpApp(Router("/" -> corsRoutes).orNotFound)
