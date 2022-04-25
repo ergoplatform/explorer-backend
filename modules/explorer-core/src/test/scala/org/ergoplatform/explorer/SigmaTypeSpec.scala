28e2da6892eccba8c4bf1edcd8307aec0059eb07
monyedavid <monyeodavid@gmail.com>
2022-04-25 21:24:34 +0100

    version-update: . ScalaTest . Updated Tests . Use scalaTestPlus

--- modules/explorer-core/src/test/scala/org/ergoplatform/explorer/SigmaTypeSpec.scala
+++ modules/explorer-core/src/test/scala/org/ergoplatform/explorer/SigmaTypeSpec.scala
@@ -3,19 +3,26 @@ package org.ergoplatform.explorer
 import cats.data.NonEmptyList
 import org.ergoplatform.explorer.SigmaType.SimpleKindSigmaType._
 import org.ergoplatform.explorer.SigmaType.{SCollection, STupleN}
-import org.scalatest.{Matchers, PropSpec}
 
-class SigmaTypeSpec extends PropSpec with Matchers {
-  property("Primitive type signature parsing") {
+import org.scalatest._
+import flatspec._
+import matchers._
+
+class SigmaTypeSpec extends AnyFlatSpec with should.Matchers {
+
+  "SigmaType parse method" should "parse primitive type signature" in {
     val sig = "SInt"
-    SigmaType.parse(sig) shouldBe Some(SInt)
+    SigmaType.parse(sig) should be(Some(SInt))
   }
-  property("HKT signature parsing") {
+
+  it should "parse HKT signature" in {
     val sig = "Coll[SInt]"
-    SigmaType.parse(sig) shouldBe Some(SCollection(SInt))
+    SigmaType.parse(sig) should be(Some(SCollection(SInt)))
   }
-  property("Nested HKT signature parsing") {
+
+  it should "parse Nested HKT signature" in {
     val sig = "Coll[(SInt,SLong)]"
-    SigmaType.parse(sig) shouldBe Some(SCollection(STupleN(NonEmptyList.of(SInt, SLong))))
+    SigmaType.parse(sig) should be(Some(SCollection(STupleN(NonEmptyList.of(SInt, SLong)))))
   }
+
 }
