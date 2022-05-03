de0cfa6703246b46d70ba089fde6a0050114ffe3
monyedavid <monyeodavid@gmail.com>
2022-05-03 10:31:28 +0100

    scalatestplus - dependencies update Missing commits (30/04/2021)

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -97,7 +97,7 @@ object dependencies {
     "com.dimafeng"               %% "testcontainers-scala-scalatest"  % TestContainersScalaVersion % Test,
     "com.dimafeng"               %% "testcontainers-scala-postgresql" % TestContainersScalaVersion % Test,
     "com.github.alexarchambault" %% "scalacheck-shapeless_1.14"       % ScalaCheckShapelessVersion % Test,
-    "org.scalatestplus"          %% "scalacheck-1-15"                 % ScalaTestPlusVersion       % "test"
+    "org.scalatestplus"          %% "scalacheck-1-15"                 % ScalaTestPlusVersion       % Test
   )
 
   val Typing: List[ModuleID] = List(
