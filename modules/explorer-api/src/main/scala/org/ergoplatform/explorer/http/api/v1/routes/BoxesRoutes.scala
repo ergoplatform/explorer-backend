a8a90c382893c46e61e010a1c841ab81efc9fece
monyedavid <monyeodavid@gmail.com>
2022-05-14 00:15:18 +0100

    Update:   - BoxRoutes   - AddressesRoutes   - RoutesV1Bundle   - HttpAPI

--- modules/explorer-api/src/main/scala/org/ergoplatform/explorer/http/api/v1/routes/BoxesRoutes.scala
+++ modules/explorer-api/src/main/scala/org/ergoplatform/explorer/http/api/v1/routes/BoxesRoutes.scala
@@ -3,11 +3,12 @@ package org.ergoplatform.explorer.http.api.v1.routes
 import cats.effect.{Concurrent, ContextShift, Timer}
 import cats.syntax.semigroupk._
 import io.chrisdavenport.log4cats.Logger
+import org.ergoplatform.ErgoAddressEncoder
 import org.ergoplatform.explorer.http.api.algebra.AdaptThrowable.AdaptThrowableEitherT
 import org.ergoplatform.explorer.http.api.syntax.adaptThrowable._
 import org.ergoplatform.explorer.http.api.syntax.routes._
 import org.ergoplatform.explorer.http.api.v1.defs.BoxesEndpointDefs
-import org.ergoplatform.explorer.http.api.v1.services.Boxes
+import org.ergoplatform.explorer.http.api.v1.services.{Boxes, Mempool}
 import org.ergoplatform.explorer.http.api.{streaming, ApiErr}
 import org.ergoplatform.explorer.settings.RequestsSettings
 import org.http4s.HttpRoutes
@@ -15,7 +16,10 @@ import sttp.tapir.server.http4s._
 
 final class BoxesRoutes[
   F[_]: Concurrent: ContextShift: Timer: AdaptThrowableEitherT[*[_], ApiErr]
-](settings: RequestsSettings, service: Boxes[F])(implicit opts: Http4sServerOptions[F, F]) {
+](settings: RequestsSettings, service: Boxes[F], mempool: Mempool[F])(implicit
+  opts: Http4sServerOptions[F, F],
+  e: ErgoAddressEncoder
+) {
 
   val defs = new BoxesEndpointDefs[F](settings)
 
@@ -140,7 +144,8 @@ object BoxesRoutes {
 
   def apply[F[_]: Concurrent: ContextShift: Timer: Logger](
     settings: RequestsSettings,
-    service: Boxes[F]
-  )(implicit opts: Http4sServerOptions[F, F]): HttpRoutes[F] =
-    new BoxesRoutes[F](settings, service).routes
+    service: Boxes[F],
+    mempool: Mempool[F]
+  )(implicit opts: Http4sServerOptions[F, F], e: ErgoAddressEncoder): HttpRoutes[F] =
+    new BoxesRoutes[F](settings, service, mempool).routes
 }
