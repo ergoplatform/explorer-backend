a2fefc1f93dcdc47cc0aac33f1f1d03e6fcce3a4
oskin1 <ilya.arcadich@gmail.com>
2022-05-18 12:45:53 +0300

    Insert qs logging.

--- modules/explorer-core/src/main/scala/org/ergoplatform/explorer/db/queries/QuerySet.scala
+++ modules/explorer-core/src/main/scala/org/ergoplatform/explorer/db/queries/QuerySet.scala
@@ -2,6 +2,7 @@ package org.ergoplatform.explorer.db.queries
 
 import cats.implicits._
 import doobie.implicits._
+import doobie.util.log.LogHandler
 import doobie.util.{Read, Write}
 import doobie.{ConnectionIO, Update}
 
@@ -17,22 +18,22 @@ trait QuerySet {
     */
   val fields: List[String]
 
-  def insert[M: Read: Write](m: M): ConnectionIO[M] =
+  def insert[M: Read: Write](m: M)(implicit lh: LogHandler): ConnectionIO[M] =
     insert.withUniqueGeneratedKeys[M](fields: _*)(m)
 
-  def insertNoConflict[M: Read: Write](m: M): ConnectionIO[Int] =
+  def insertNoConflict[M: Read: Write](m: M)(implicit lh: LogHandler): ConnectionIO[Int] =
     insertNoConflict.run(m)
 
-  def insertMany[M: Read: Write](xs: List[M]): ConnectionIO[List[M]] =
+  def insertMany[M: Read: Write](xs: List[M])(implicit lh: LogHandler): ConnectionIO[List[M]] =
     insert.updateManyWithGeneratedKeys[M](fields: _*)(xs).compile.to(List)
 
-  def insertManyNoConflict[M: Read: Write](xs: List[M]): ConnectionIO[Int] =
+  def insertManyNoConflict[M: Read: Write](xs: List[M])(implicit lh: LogHandler): ConnectionIO[Int] =
     insertNoConflict.updateMany(xs)
 
-  private def insert[M: Write]: Update[M] =
+  private def insert[M: Write](implicit lh: LogHandler): Update[M] =
     Update[M](s"insert into $tableName ($fieldsString) values ($holdersString)")
 
-  private def insertNoConflict[M: Write]: Update[M] =
+  private def insertNoConflict[M: Write](implicit lh: LogHandler): Update[M] =
     Update[M](s"insert into $tableName ($fieldsString) values ($holdersString) on conflict do nothing")
 
   private def fieldsString: String =
