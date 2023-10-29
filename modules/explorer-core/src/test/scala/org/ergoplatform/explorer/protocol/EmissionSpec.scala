e9d0a2d20d5570c9a12ed150e0fea6924a7d17e2
oskin1 <ilya.arcadich@gmail.com>
2023-10-29 15:30:22 +0300

    Disable file logging by default.

--- modules/explorer-core/src/test/scala/org/ergoplatform/explorer/protocol/EmissionSpec.scala
+++ modules/explorer-core/src/test/scala/org/ergoplatform/explorer/protocol/EmissionSpec.scala
@@ -8,6 +8,6 @@ class EmissionSpec extends AnyFlatSpec with should.Matchers {
   val emission = new Emission(MonetarySettings(), ReemissionSettings())
   "Emission.issuedCoinsAfterHeight" should "compute correct total supply" in {
     println((1L to 1119273L).map(h => emission.emissionAt(h)).sum)
-    println(emission.issuedCoinsAfterHeight(1119273L))
+    println(emission.issuedCoinsAfterHeight(8000000))
   }
 }
